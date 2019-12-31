a025111c8b9cccf76a3fd822d287b57182c00300
lule <1721753977@qq.com>
2019-12-31 11:08:26 +0800

    commit anther line to Test.java

--- com/test/Test.java
+++ com/test/Test.java
@@ -1,6 +1,7 @@
 public class Test{
 
 	public void static void main(String args[]){
+	       System.out.println("hello-world-china....");  
 	       System.out.println("hello-world-china....");
 	}
 
