54ccf404082b6aa15670d5f97d0b6daf4fcf7f2e
lule <1721753977@qq.com>
2019-12-31 11:15:59 +0800

    commit anther line without ...

--- com/test/Test.java
+++ com/test/Test.java
@@ -1,7 +1,8 @@
 public class Test{
 
 	public void static void main(String args[]){
-	       System.out.println("hello-world-china....");
+	       System.out.println("hello-world-china...."); 
+	       System.out.println("hello-world-china");
 	}
 
 }	
